099cd05cfd3cba78333f02690a5976c4838a199c
Sai Vennela Garikapati <saivennela.garikapati@sjsu.edu>
2022-11-29 10:27:59 -0800

    Integrate ML model with Web App using Flask.

---
+++ code/WebApp/model.py
@@ -0,0 +1,87 @@
+### Model.py
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+import collections, numpy
+import numpy as np, pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import confusion_matrix, accuracy_score
+from nltk.stem import PorterStemmer
+import spacy
+import enchant
+import re
+
+#Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+# print(model.predict(["global warm report urg govern belgium world face increas hunger"]))
+
+def spacyPipeline(tweets):
+    ps = PorterStemmer()
+    nlp = spacy.load('en_core_web_sm')
+    
+    preprocessed_tweets = []
+    for t in tweets:
+        doc = nlp(t)
+        filtered_tweet = []
+        
+        for token in doc:
+            if (not token.is_stop) and token.is_alpha:
+                filtered_tweet.append(ps.stem(str(token)))
+        
+        preprocessed_tweets.append(filtered_tweet)
+    
+    return preprocessed_tweets
+
+def preprocess(tweets):
+
+    whiteList=['climate','change','earth','global','warming','planet']
+
+    #Remove mentions
+    count=0
+    mentions_tweets = [re.findall('@\w+', tweet) for tweet in tweets]
+
+    #Remove hash sign
+    hash_tweets = [re.findall('#\w+', tweet) for tweet in tweets]
+
+    #Remove urls
+    urls_tweets = [re.findall(r'http.?://[^\s]+[\s]?', tweet) for tweet in tweets]
+
+
+    #Convert all to lowercase
+    tweets = [t.lower() for t in tweets]
+    
+    #Process tweets through spaCy pipeline
+    tweets = spacyPipeline(tweets)
+    
+    #Filter out words
+    tweets = [list(filter(lambda w: w != 'link', t)) for t in tweets]
+    
+    #Remove words less than length 2
+    tweets = [list(filter(lambda w: len(w) > 2, t)) for t in tweets]
+
+    # print(tweets)
+    return tweets
+
+def sent_analysis(tweet: str) -> str:
+    
+    d = enchant.Dict("en_US")
+    
+    for tw in tweet:
+        if(d.check(tw)):
+            return 
+        elif:
+            return 
+      tCount++;
+        
+    tweet = preprocess([tweet])
+    tweet = [" ".join(t) for t in tweet]
+    return model.predict(tweet)
